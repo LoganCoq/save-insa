81cdcd69dfda47ba3308c0916b19247ae94713c9
Logan Coquérant <logan.coquerant@insa-cvl.fr>
2017-12-07 16:09:29 +0100

    progSys ls -lh

--- S5/progSys/TD_FS/new_ls.c
+++ S5/progSys/TD_FS/new_ls.c
@@ -8,64 +8,85 @@
 #include <stdlib.h>
 #include <string.h>
 
-int new_ls(struct stat info){
+int new_ls(struct stat *info, const char *ref){
         char type;
-	char pws[9], grs[9];
-	struct passwd * pw = getpwuid(info.st_uid);
-        if ( pw != NULL )
-        {
-		strcpy(pws, pw->pw_name);
-        } else
-        {
-        	printf("%li\n", info.st_uid);
-        }
-        struct group * gr = getgrgid(info.st_gid);
-        if ( gr != NULL )
-        {
-        	strcpy(grs, gr->gr_name);
-        } else {
-        	printf("%s\n", info.st_gid);
-        }        
-        if ( S_ISREG(info.st_mode)) { type = '-'; }
-        else if ( S_ISDIR(info.st_mode) ) { type = 'd'; }
-        else if ( S_ISCHR(info.st_mode) ) { type = 'c'; }
-        else if ( S_ISBLK(info.st_mode) ) { type = 'b'; }
-        else if ( S_ISFIFO(info.st_mode) ) { type = 'f'; }
-        else if ( S_ISLNK(info.st_mode) ) { type = 'l'; }
-        else if ( S_ISSOCK(info.st_mode) ) { type = 's'; }
+	#define LTEMPS 32
+	char pws[9], grs[9], temps[LTEMPS], curfile[1024];
+	struct passwd * pw = getpwuid(info->st_uid);
+        if ( pw != NULL ) strcpy(pws, pw->pw_name);
+        else printf("%8d\n", info->st_uid);
 
-	printf("%c%c%c%c%c%c%c%c%c%c %s %s %li\n",
+        struct group * gr = getgrgid(info->st_gid);
+        if ( gr != NULL ) strcpy(grs, gr->gr_name);
+        else printf("%8d\n", info->st_gid);
+        
+	strcpy(curfile, ref);
+
+	type = '?';
+        if ( S_ISREG(info->st_mode)) { type = '-'; }
+        else if ( S_ISDIR(info->st_mode) ) { type = 'd'; }
+        else if ( S_ISCHR(info->st_mode) ) { type = 'c'; }
+        else if ( S_ISBLK(info->st_mode) ) { type = 'b'; }
+        else if ( S_ISFIFO(info->st_mode) ) { type = 'f'; }
+        else if ( S_ISLNK(info->st_mode) ) { 
+		type = 'l'; 
+		char buf[1024];
+		char *res;
+		if ( (res = realpath(ref, buf)))
+		{
+			strcat(curfile, " -> ");
+			strcat(curfile, buf);
+		}
+	}
+        else if ( S_ISSOCK(info->st_mode) ) { type = 's'; }
+	
+	strftime(temps, LTEMPS, "%e %h %Y %H:%M", localtime(&info->st_mtime));
+
+	printf("%c%c%c%c%c%c%c%c%c%c %8s %8s %li %s %s\n",
 		type,
-		info.st_mode&S_IRUSR?'r':'-',
-		info.st_mode&S_IWUSR?'w':'-',
-		info.st_mode&S_IXUSR?'x':'-',
-		info.st_mode&S_IRGRP?'r':'-',
-                info.st_mode&S_IWGRP?'w':'-',
-                info.st_mode&S_IXGRP?'x':'-',
-		info.st_mode&S_IROTH?'r':'-',
-                info.st_mode&S_IWOTH?'w':'-',
-                info.st_mode&S_IXOTH?'x':'-',
+		info->st_mode&S_IRUSR?'r':'-',
+		info->st_mode&S_IWUSR?'w':'-',
+		info->st_mode&S_IXUSR?'x':'-',
+		info->st_mode&S_IRGRP?'r':'-',
+                info->st_mode&S_IWGRP?'w':'-',
+                info->st_mode&S_IXGRP?'x':'-',
+		info->st_mode&S_IROTH?'r':'-',
+                info->st_mode&S_IWOTH?'w':'-',
+                info->st_mode&S_IXOTH?'x':'-',
 		pws,
 		grs,
-		info.st_size
-		
+		info->st_size,
+		temps,
+		curfile
 		);
-	exit(EXIT_SUCCESS);
+	return 0;
 }
 
 int main(int argc, char ** argv)
 {
 	struct stat info;
 	char type;
-	if ( stat(argv[1], &info) != -1 )
+	if ( argc < 2 )
 	{
-		new_ls(info);
-		exit(EXIT_SUCCESS);		
+		printf("Wrong usage of cmd. Usage ./ls <files>");
+		exit(EXIT_FAILURE);
 	}
 	else
 	{
-		perror("no such path"); 
-		exit(EXIT_FAILURE);
+		int i;
+		for ( i=1; i<argc; i++)
+		{
+			if ( lstat(argv[i], &info) != -1 )
+			{
+				new_ls(&info, argv[i]);
+			}
+			else
+			{
+				perror("no such path"); 
+				exit(EXIT_FAILURE);
+			}
+		}
+		exit(EXIT_SUCCESS);
 	}
 }
 
